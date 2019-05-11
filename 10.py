38900b4da83d838b653b6f4c08ae028c5fb79ff1
Divyaswor Makai <makaidivya@gmail.com>
2019-05-11 14:39:34 +0545

    Revert "added question and 29th prob"
    This reverts commit da69193b74233b56f033db6c5b8cb87df0ddf470.

--- 10.py
+++ 10.py
@@ -1,15 +1,11 @@
-# Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
-
 import time
 
 def JobScheduler(printMsg,waitTime):
 	waitTime = waitTime/1000
 	printMsg()
 	time.sleep(waitTime)
-	JobScheduler(printMsg,5000)
 
 def printMsg():
 	print("Done")
 
-JobScheduler(printMsg,5000)
-
+JobScheduler(printMsg)
--- 2.py
+++ 2.py
@@ -1,11 +1,3 @@
-# Given an array of integers, return a new array such that each element at index i of the new array is
-# the product of all the numbers in the original array except the one at i.
-
-# For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. 
-# If our input was [3, 2, 1], the expected output would be [2, 3, 6].
-
-# Follow-up: what if you can't use division?
-
 a=[]
 b=[]
 prod =1
@@ -16,8 +8,6 @@ while 1:
 		prod =prod*int(x)
 	else:
 		break
-
-# a = list(map(int,input().split(' ')))  -----for direct array input but have to use space for reasons
 # method 1
 # for var in a:
 # 	temp = prod/var
